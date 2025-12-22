07003448a7bd2cbb919401087b8e37b93433e476
Sophanith Mey <sophanith.mey@wingbank.com.kh>
2025-12-22 15:51:56 +0700

    chore: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Work_Sans } from "next/font/google";
-import "@/app/globals.css";
-import { cn } from "@/lib/utils";
-import { MyFirebaseProvider } from "@/components/firebase-providers";
-import { Toaster } from "@/components/ui/toaster";
-import { ReactNode } from "react";
+import type { Metadata } from 'next';
+import { Work_Sans } from 'next/font/google';
+import '@/app/globals.css';
+import { cn } from '@/lib/utils';
+import { MyFirebaseProvider } from '@/components/firebase-providers';
+import { Toaster } from '@/components/ui/toaster';
+import { ReactNode } from 'react';
 
-const font = Work_Sans({ subsets: ["latin"] });
+const font = Work_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Venefish | Vercel Next.JS Firebase Shadcn/ui Tailwind Boilerplate",
-  description:
-    "Venefish is a Vercel Next.JS Firebase Shadcn/ui Tailwind Boilerplate project to help you get started with your next project.",
+  title: 'Police Alert Map',
+  description: 'Discover and explore police alerts pinned across Cambodia.',
+  keywords: 'police, alerts, map, nextjs, firebase',
+  viewport: 'width=device-width, initial-scale=1.0',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={cn(font.className)}>
         <MyFirebaseProvider>
           {children}
